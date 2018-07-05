0f433a99818fdcc012fe6944469d0f9bbb87a2e4
Annie Shin, Rita Bennett-Chew and Will Swanson <annie@>
2018-07-05 09:04:38 -0700

    add content for input of favorite things

--- app.rb
+++ app.rb
@@ -3,11 +3,13 @@ require('sinatra/reloader')
 also_reload('lib/**/*.rb')
 require('./lib/favorite_things')
 
+#home page is the /
 get('/') do
   @list = Item.all()
   erb(:list)
 end
 
+#sending information
 post('/') do
   name = params["name"]
   item = Item.new(name)
@@ -16,6 +18,7 @@ post('/') do
   erb(:list)
 end
 
+#gets information from items page
 get('/items/:id') do
   @item = Item.find(params[:id])
   erb(:item)
